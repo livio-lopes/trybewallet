e82884da4353fc2c11243fa59f8266d48d5092b0
Lívio Lopes <antonio.lvio760@gmail.com>
2023-04-11 19:15:27 -0300

    Requisito 3
    wallet reducer configurado com fetch

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,3 +1,5 @@
+import { fetchListCoins } from '../../services/fetchAPI';
+
 export const SAVE_EMAIL = 'SAVE_EMAIL';
 
 export const saveEmail = (email) => ({
@@ -6,3 +8,31 @@ export const saveEmail = (email) => ({
     email,
   },
 });
+
+export const FETCH_LIST_LOAD = 'FETCH_LIST_LOAD';
+export const FETCH_LIST_SUCCESS = 'FETCH_LIST_SUCCESS';
+export const FETCH_LIST_ERROR = 'FETCH_LIST_ERROR';
+
+const fetchListLoad = () => ({
+  type: FETCH_LIST_LOAD,
+});
+
+const fetchListError = (error) => ({
+  type: FETCH_LIST_ERROR,
+  payload: error,
+});
+
+const fetchListSuccess = (currencies) => ({
+  type: FETCH_LIST_SUCCESS,
+  payload: { currencies },
+});
+
+export const actionFetchListCoint = () => async (dispatch) => {
+  dispatch(fetchListLoad());
+  try {
+    const list = await fetchListCoins();
+    dispatch(fetchListSuccess(list));
+  } catch (error) {
+    dispatch(fetchListError(error));
+  }
+};
--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,12 +1,36 @@
+import {
+  FETCH_LIST_LOAD,
+  FETCH_LIST_SUCCESS,
+  FETCH_LIST_ERROR } from '../actions';
+
 const INITIAL_STATE = {
   currencies: [],
   expenses: [],
   editor: false,
   idToEdit: 0,
+  isLoading: false,
+  errorMsg: null,
 };
 
 const wallet = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+  case FETCH_LIST_LOAD:
+    return {
+      ...state,
+      isLoading: true,
+    };
+  case FETCH_LIST_SUCCESS:
+    return {
+      ...state,
+      isLoading: false,
+      currencies: [...state.currencies, action.payload.currencies],
+    };
+  case FETCH_LIST_ERROR:
+    return {
+      ...state,
+      isLoading: false,
+      errorMsg: action.payload,
+    };
   default:
     return state;
   }
