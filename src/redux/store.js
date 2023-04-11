244c2961e318ebc683c294e84ac118d1d37a37dd
Lívio Lopes <antonio.lvio760@gmail.com>
2023-04-11 16:14:35 -0300

    Requisito 1
    Configurando Redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,11 @@
-// configure aqui sua store
+import { legacy_createStore as createStore } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
+import rootReducers from './reducers';
+
+const store = createStore(rootReducers, composeWithDevTools());
+
+if (window.Cypress) {
+  window.store = store;
+}
+
+export default store;
