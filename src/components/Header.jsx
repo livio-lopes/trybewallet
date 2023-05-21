316c61b0995b16e214905ebbf43ea7aa80cb65c3
Lívio Lopes <antonio.lvio760@gmail.com>
2023-05-21 16:03:50 -0300

    Atualizando CSS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,10 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Logo from './Logo';
+import styles from './Header.module.css';
+import expense from '../images/expense.svg';
+import person from '../images/person.svg';
 
 class Header extends Component {
   totalFiled = (expenses) => {
@@ -15,14 +19,44 @@ class Header extends Component {
   render() {
     const { email, expenses } = this.props;
     return (
-      <div>
-        <h1 data-testid="email-field">{email}</h1>
-        <h2 data-testid="total-field">
-          {expenses.length !== 0
-            ? this.totalFiled(expenses).toFixed(2) : '0.00'}
+      <div className={ styles.container }>
+        <Logo className={ styles.logo_header } />
+        <span className={ styles.total__expense }>
+          <img
+            className={ styles.icon__expense }
+            src={ expense }
+            alt="icone_despesa"
+          />
+          Total de despesas:
+          <span
+            className={ styles.expense }
+            data-testid="total-field"
+          >
+            {expenses.length !== 0
+              ? this.totalFiled(expenses).toFixed(2) : '0.00'}
 
-        </h2>
-        <h2 data-testid="header-currency-field">BRL</h2>
+          </span>
+          <span
+            className={ styles.expense }
+            data-testid="header-currency-field"
+          >
+            BRL
+
+          </span>
+        </span>
+        <span
+          className={ styles.email }
+          data-testid="email-field"
+        >
+          <img
+            className={ styles.icon__person }
+            src={ person }
+            alt="icone_despesa"
+          />
+          {email}
+
+        </span>
+
       </div>
     );
   }
